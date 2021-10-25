9c8f89f465c301083b40480b540c4e3c8ff5cd23
Zach Youssef <zacharysamiyoussef@gmail.com>
2021-10-25 09:02:50 -0400

    Begin implementing ls

--- fslayer/app/src/main/java/foundationdb_fslayer/fuse/FuseLayer.java
+++ fslayer/app/src/main/java/foundationdb_fslayer/fuse/FuseLayer.java
@@ -1,5 +1,6 @@
 package foundationdb_fslayer.fuse;
 
+import com.apple.foundationdb.directory.DirectoryLayer;
 import foundationdb_fslayer.fdb.FoundationFileOperations;
 import jnr.ffi.Pointer;
 import ru.serce.jnrfuse.ErrorCodes;
@@ -8,23 +9,28 @@ import ru.serce.jnrfuse.FuseStubFS;
 import ru.serce.jnrfuse.struct.FileStat;
 import ru.serce.jnrfuse.struct.FuseFileInfo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FuseLayer extends FuseStubFS {
 
   private final FoundationFileOperations dbOps;
+  private final DirectoryLayer dir;
 
   public FuseLayer(FoundationFileOperations dbOps) {
     this.dbOps = dbOps;
+    this.dir = new DirectoryLayer();
   }
 
   @Override
   public int getattr(String path, FileStat stat) {
     int res = 0;
-    if(path.equals("/")){
+
+    if (dbOps.ls(dir, Arrays.asList(path.split("/"))) != null){
       stat.st_mode.set(FileStat.S_IFDIR | 0755);
       stat.st_nlink.set(2);
-    } else {
-      res = -ErrorCodes.ENOENT();
     }
+
     return res;
   }
 
@@ -34,9 +40,21 @@ public class FuseLayer extends FuseStubFS {
   }
 
   @Override
+  public int opendir(String path, FuseFileInfo fi) {
+    return 0;
+  }
+
+  @Override
   public int readdir(String path, Pointer buf, FuseFillDir filter, long offset, FuseFileInfo fi) {
-    filter.apply(buf, ".", null, 0);
-    filter.apply(buf, "..", null, 0);
+    List<String> contents = dbOps.ls(dir, Arrays.asList(path.split("/")));
+
+    if (contents != null) {
+      filter.apply(buf, ".", null, 0);
+      filter.apply(buf, "..", null, 0);
+
+      contents.forEach(item -> filter.apply(buf, item, null, 0));
+    }
+
     return 0;
   }
 }
